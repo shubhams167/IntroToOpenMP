8cee2692fcfb644e58e7003b292db1b2e4cd1ce2
Shubham Singh <shubhams.167@gmail.com>
2019-09-15 15:20:49 +0530

    ArraySum.c has been added
    This file contains code to sum up values of an array in parallel

---
+++ OMP - ArraySum.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+
+int main(int argc, char *argv[]) {
+    int SIZE = 1000000000;
+    int i, sum = 0;
+	
+	//Allocate memory for array in the heap
+    int *arr = (int*)malloc(sizeof(int)*SIZE);
+    
+	//Fill array with all 1's
+	for (i = 0; i < SIZE; i++) 
+		arr[i] = 1;
+
+	//Get current time
+    double start = omp_get_wtime();
+
+	//Parallel region starts from here
+	#pragma omp parallel for default(shared) reduction(+: sum)
+		for (i = 0; i <= SIZE; i++)
+			sum += arr[i];
+	//Parallel region ends here
+
+	//Get current time
+	double end = omp_get_wtime();
+    
+	printf("Sum = %d, time = %.10lf", sum, end - start);
+	
+	//Free allocated memory
+	free(arr);
+    return 0;
+}
